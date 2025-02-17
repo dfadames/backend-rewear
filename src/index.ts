c66ee9d25b67a0a03c397e3d7d94ce29e56db4b8
dfadames <dadames@unal.edu.co>
2025-02-17 17:59:26 -0500

    2.5.2 google auth working

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ app.use(cors(corsOptions));
 const PORT = process.env.PORT;
 
 //importamos el direccionamiento de rutas:
-import { login, register } from "./controllers/authController";
+import { login, register, googleAuth } from "./controllers/authController";
 import { ping, getUsuarios } from "./controllers/othersController";
 import { authenticateToken } from "./token/authtoken";
 import { getProfileInfo, getUserProfileByUsername , getUserProfileById, getuseridByUsername} from "./controllers/profileController";
@@ -47,7 +47,7 @@ const getProductData = [getProductInfo];
 //rutas de autenticacion de credenciales
 app.post("/login", login);
 app.post("/register", register);
-
+app.post("/auth/google", googleAuth); // A
 //rutas de funcionalidades varias:
 app.get("/ping", ping);
 app.get("/usuarios", getUsuarios);
